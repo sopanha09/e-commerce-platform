4be4b5bd73a3a916823161323c0ac4f9087cb4bb
sopanha09 <sopanhasous@gmail.com>
2024-01-20 14:39:48 +0700

    qty & add to cart handler

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -9,12 +9,13 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      const item = state.payload;
+      const item = action.payload;
+      console.log("item", action.payload);
 
       const existItem = state.cartItems.find((x) => x._id === item._id);
 
       if (existItem) {
-        state.cartItems = state.cartItems.find((x) =>
+        state.cartItems = state.cartItems.map((x) =>
           x._id === existItem._id ? item : x
         );
       } else {
@@ -23,7 +24,7 @@ const cartSlice = createSlice({
 
       // Calculate items price
       state.itemsPrice = state.cartItems
-        .reduce((acc, item) => acc + item.price * item.qty, 0)
+        .reduce((acc, item) => acc + item?.price * item?.qty, 0)
         .toFixed(2);
 
       // Calculate shipping price (If order is over $100 then free, else $10 shipping)
