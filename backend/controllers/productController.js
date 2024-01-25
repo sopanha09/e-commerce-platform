08299de4396a3204e919665e8867b2f3facc94d8
sopanha09 <sopanhasous@gmail.com>
2024-01-25 12:07:55 +0700

    reset

---
+++ backend/controllers/productController.js
@@ -0,0 +1,70 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+// @desc Fetch a product
+// @route GET /api/products/:id
+// @access Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+
+// @desc  Create products
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// @desc update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.status(200).json(updateProduct);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+export { getProducts, getProductById, createProduct, updateProduct };
---
+++ frontend/README.md
@@ -0,0 +1,9 @@
+#What's new?
+
+- Starting the Frontend (2):
+  - Header & Footer Components
+  - Custom Bootstrap Theme
+  - List Products
+  - Implement React Router
+  - Rating Components
+  - Product Details Page
